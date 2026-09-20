cf5d1babca1f44f7abf08c274e58e83e3e134876
ybey6891-lab <ybey6891@gmail.com>
2026-09-20 17:47:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,21 +16,37 @@ wss.on('connection', (ws) => {
             return;
         }
 
-        if (data.type === 'join') {
+        // 1. عندما يقوم الـ Host بإنشاء الغرفة
+        if (data.type === 'create') {
             currentRoom = data.room;
             peerId = data.id;
-            if (!rooms[currentRoom]) {
-                rooms[currentRoom] = {};
+            rooms[currentRoom] = {};
+            rooms[currentRoom][peerId] = ws;
+            ws.send(JSON.stringify({ type: 'created', code: currentRoom }));
+        } 
+        // 2. عندما يحاول الـ Join الانضمام
+        else if (data.type === 'join') {
+            currentRoom = data.room;
+            peerId = data.id;
+
+            // التحقق الذكي: إذا لم تكن الغرفة موجودة أصلاً (أنشأها Host)، ارفض الطلب فوراً!
+            if (!rooms[currentRoom] || Object.keys(rooms[currentRoom]).length === 0) {
+                ws.send(JSON.stringify({ type: 'error', message: 'Room not found or invalid code!' }));
+                return;
             }
+
             rooms[currentRoom][peerId] = ws;
 
+            // ربط اللاعبين ببعضهما
             for (let id in rooms[currentRoom]) {
                 if (id !== peerId) {
                     rooms[currentRoom][id].send(JSON.stringify({ type: 'peer_joined', id: peerId }));
                     ws.send(JSON.stringify({ type: 'peer_joined', id: id }));
                 }
             }
-        } else if (data.type === 'signal') {
+        } 
+        // 3. إشارات الـ WebRTC المتبادلة
+        else if (data.type === 'signal') {
             if (currentRoom && rooms[currentRoom] && rooms[currentRoom][data.to]) {
                 rooms[currentRoom][data.to].send(JSON.stringify({
                     type: 'signal',
@@ -55,4 +71,3 @@ wss.on('connection', (ws) => {
 });
 
 console.log(`Signaling server running on port ${PORT}`);
-
